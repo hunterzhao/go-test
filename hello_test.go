95fe38cb77c3c781a69410f8c5d38de2818eb697
montyzhao <montyzhao@tencent.com>
2021-02-21 10:38:26 +0800

    cov

--- hello_test.go
+++ hello_test.go
@@ -13,7 +13,7 @@ func TestMin(t *testing.T) {
 
 func TestMinErr(t *testing.T) {
    a := 2
-   b := 1
+   b := 3
    res := Min(a, b)
    if res == false {
        t.Errorf("test error, %d, %d", a, b)
